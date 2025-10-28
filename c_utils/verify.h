0ff29ae4a0d683795f4412aae4f4c512c5d554d0
NiallWalker2012 <niallwalker2025@gmail.com>
2025-10-28 14:07:32 +0000

    Added functions to verify.h

--- c_utils/verify.h
+++ c_utils/verify.h
@@ -3,6 +3,8 @@
 #ifndef VERIFY_H
 #define VERIFY_H
 
+static long get_file_size(const char *filename);
+int compute_sha256(const char *filename, unsigned char hash[EVP_MAX_MD_SIZE], unsigned int *hash_len, long max_bytes);
 bool verify(const char *iso_path, const char *dev_path);
 
 #endif
